f0da484ca47db8a35bf08bc1df0be34bb7e600ca
Tirzah P. Howard <tp.howard47@gmail.com>
2020-09-18 02:47:42 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,11 +2,12 @@ def hello_t(array)
   i = 0
 
   while i < array.length
-    yield array[i]
+    yield(array[i])
     i = i + 1
   end
-end
 
+  array
+end
 # call your method here!
 ["Tim", "Tom", "Jim"]
 
